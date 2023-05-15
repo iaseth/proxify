4c3f3d810dba911f19279e8a834c735b29046358
Ankur Seth <iankurseth@gmail.com>
2023-05-15 18:01:26 +0530

    Deployed to netlify

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,8 +7,8 @@ function App () {
 	return (
 		<div className="min-h-screen bg-green-100 text-white flex">
 			<a href="https://github.com/iaseth/vite-react-ts-starter" className="m-auto bg-green-600 text-center px-8 py-6 rounded">
-				<h1 className="H3">Start</h1>
-				<p className="read-the-docs">vite-react-ts-starter</p>
+				<h1 className="H3">Proxify</h1>
+				<p className="read-the-docs">Netlify as a Proxy Server</p>
 			</a>
 		</div>
 	);
